cccd6b253a515a8f672aebd73d0512e172f9bcd3
Maphnew <jinssakura@gmail.com>
2019-03-07 23:26:11 +0900

    movie-#2props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie.js';
+
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg"
+  },
+  {
+    title: "Full Metal Jacket",
+    poster: "https://ae01.alicdn.com/kf/HTB19B5SQXXXXXafXXXXq6xXFXXXe/J1354-FULL-METAL-JACKET-Stanley-Kubrick-Vietnam-War-Movie-Pop-14x21-24x36-Inches-Silk-Art-Poster.jpg"
+  },
+  {
+    title: "Oldboy",
+    poster: "http://www.languagetrainers.com/reviews/foreign-film-reviews/uploads/9214-Oldboy.jpg"
+  },
+  {
+    title: "Star Wars",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/914-XkqbGZL._SX342_.jpg"
+  }
+
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {movies.map((movie, index) => {
+          return <Movie title={movie.title} poster={movie.poster} key={index}/>
+        })}
       </div>
     );
   }
